f40a0884b7fd9230c76898e6de054978b9e68d44
tinmn9088 <tinman.8.9@yandex.ru>
2022-06-21 11:44:41 +0300

    feat(lab03-2): wrote models and resource files

---
+++ lab03-2/src/main/java/by/vsu/flat/Flat.java
@@ -0,0 +1,44 @@
+package by.vsu.flat;
+
+public class Flat {
+
+    private String number;
+
+    private String entrance;
+
+    private String floor;
+
+    private String owner;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEntrance() {
+        return entrance;
+    }
+
+    public void setEntrance(String entrance) {
+        this.entrance = entrance;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
---
+++ lab03-2/src/main/java/by/vsu/request/Request.java
@@ -0,0 +1,48 @@
+package by.vsu.request;
+
+import by.vsu.flat.Flat;
+
+import java.time.LocalDate;
+
+public class Request {
+
+    private String flatNumber;
+
+    private LocalDate date;
+
+    private String description;
+
+    private int hours;
+
+    public String getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+}
