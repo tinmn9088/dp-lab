06954d8136402bf0f574756bc36c64a554f8f816
tinmn9088 <tinman.8.9@yandex.ru>
2022-06-24 11:06:40 +0300

    feat(lab05): started writing main

--- lab05/src/main/java/by/vsu/Main.java
+++ lab05/src/main/java/by/vsu/Main.java
@@ -1,8 +1,32 @@
 package by.vsu;
 
+import by.vsu.controller.EnrolleeController;
+import by.vsu.controller.processors.ListUserProcessor;
+import by.vsu.dao.EnrolleeDao;
+import by.vsu.dao.EnrolleeDaoImpl;
+import by.vsu.service.EnrolleeService;
+import by.vsu.service.EnrolleeServiceImpl;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Абитуриенты:");
+
+        // processors
+        EnrolleeDao enrolleeDao = new EnrolleeDaoImpl();
+        EnrolleeService enrolleeService = new EnrolleeServiceImpl(enrolleeDao);
+        ListUserProcessor listUserProcessor = new ListUserProcessor(enrolleeService);
+
+        EnrolleeController enrolleeController = new EnrolleeController(listUserProcessor);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(">>> ");
+        String userInput = scanner.nextLine();
+        do {
+            enrolleeController.process(userInput);
+            System.out.print(">>> ");
+            userInput = scanner.nextLine();
+        } while (!userInput.equals("exit"));
     }
 }
