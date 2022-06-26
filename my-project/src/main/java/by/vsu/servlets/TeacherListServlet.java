ae47297eff9ad8d1e97907ef3b37a2590f54a6ec
tinmn9088 <tinman.8.9@yandex.ru>
2022-06-27 00:46:32 +0300

    feat(project): changed jsp path in teacher list servlet

--- my-project/src/main/java/by/vsu/servlets/TeacherListServlet.java
+++ my-project/src/main/java/by/vsu/servlets/TeacherListServlet.java
@@ -21,7 +21,7 @@ public class TeacherListServlet extends HttpServlet {
             throws ServletException, IOException {
         List<Teacher> teachers = teacherService.getAllTeachers();
         req.setAttribute("teachers", teachers);
-        getServletContext().getRequestDispatcher("/WEB-INF/jsp/teachers.jsp")
+        getServletContext().getRequestDispatcher("/WEB-INF/jsp/teachers/list.jsp")
                 .forward(req, resp);
     }
 }
