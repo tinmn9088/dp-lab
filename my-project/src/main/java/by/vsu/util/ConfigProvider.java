58fd1130f9c9cfd66447cbb206f18388612466fa
tinmn9088 <tinman.8.9@yandex.ru>
2022-06-26 20:45:27 +0300

    refactor(project): made config provider use file class

--- my-project/src/main/java/by/vsu/util/ConfigProvider.java
+++ my-project/src/main/java/by/vsu/util/ConfigProvider.java
@@ -1,5 +1,6 @@
 package by.vsu.util;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,10 +8,10 @@ import java.util.Properties;
 
 public class ConfigProvider {
     
-    private static final String fileAdress = "src/main/webapp/WEB-INF/config.properties";
+    private static File file = new File("src/main/webapp/WEB-INF/config.properties");
 
     public static String getValue(String name) {
-        try (InputStream is = new FileInputStream(fileAdress)) {
+        try (InputStream is = new FileInputStream(file)) {
             Properties prop = new Properties();
             prop.load(is);
             return prop.getProperty(name);
