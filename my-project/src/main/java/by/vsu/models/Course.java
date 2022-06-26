1ea5ffe7ce6e60baedf1ebc82bde67f441f4897e
tinmn9088 <tinman.8.9@yandex.ru>
2022-06-26 18:12:49 +0300

    feat(models): finished

---
+++ my-project/src/main/java/by/vsu/models/Course.java
@@ -0,0 +1,46 @@
+package by.vsu.models;
+
+import lombok.Data;
+
+@Data
+public class Course {
+
+    private String id;
+    
+    private Teacher teacher;
+
+    private String title;
+
+    private String speciality;
+
+    private int semester;
+
+    private int numberOfStudents;
+
+    private int hoursOfLectures;
+
+    private int hoursOfPractice;
+
+    private int hoursOfLaboratoryWork;
+
+    private boolean exam;
+
+    private int numberOfTests;
+
+    public int getCourse() {
+        return (int) Math.ceil(((double) getSemester()) / 2);
+    }
+
+    public int getHours() {
+        double result = getHoursOfLectures() + getHoursOfPractice() + getHoursOfLaboratoryWork();
+        result += getHoursOfLectures() * 0.05;
+        result += 2;
+        if (exam) {
+            result += getNumberOfStudents() * 0.5;
+        } else {
+            result += getNumberOfStudents() * 0.25;
+        }
+        result += getNumberOfTests() * getNumberOfStudents() * 0.15;
+        return (int) Math.ceil(result);
+    }
+}
---
+++ my-project/src/main/java/by/vsu/models/Teacher.java
@@ -0,0 +1,27 @@
+package by.vsu.models;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class Teacher {
+
+    private String id;
+
+    private String lname;
+
+    private String fname;
+
+    private String patronymic;
+
+    private String degree;
+
+    private String place;
+
+    private String gender;
+
+    private LocalDate birthdate;
+
+    private int numberOfCourses;
+}
