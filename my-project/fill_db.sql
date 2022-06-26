c2a4a6a237fc77d6f9057511ffbdf71e8bbe41fb
tinmn9088 <tinman.8.9@yandex.ru>
2022-06-26 19:09:09 +0300

    fix(project): bad cyrillic characters in fill_db.sql

--- my-project/fill_db.sql
+++ my-project/fill_db.sql
@@ -4,11 +4,11 @@ START TRANSACTION;
 INSERT INTO `teachers`
 (`id`, `fname`, `lname`, `patronymic`, `degree`, `place`, `gender`, `birthdate`)
 VALUES
-(1, 'Алексей', 'Смирнов', 'Александрович', 'магистр', 'преподаватель', 'м', '1977-7-04');
+(1, N'Алексей', N'Смирнов', N'Александрович', N'магистр', N'преподаватель', N'м', '1977-7-04');
 
 INSERT INTO `courses`
 (`id`, `teacher_id`, `title`, `speciality`, `semester`, `number_of_students`, `hours_of_lectures`, `hours_of_practice`, `hours_of_lab`, `exam`)
 VALUES
-(1, 1, 'Тестовый курс', 'Спец', 4, 12, 64, 60, 56, TRUE);
+(1, 1, N'Тестовый курс', N'Спец', 4, 12, 64, 60, 56, true);
 
 COMMIT;
