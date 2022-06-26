397527bd7e84f5dcb166dccf92d8005a8f8171cd
tinmn9088 <tinman.8.9@yandex.ru>
2022-06-26 19:22:49 +0300

    feat(project): added no args constructor for models

--- my-project/src/main/java/by/vsu/models/Course.java
+++ my-project/src/main/java/by/vsu/models/Course.java
@@ -2,8 +2,10 @@ package by.vsu.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Course {
 
